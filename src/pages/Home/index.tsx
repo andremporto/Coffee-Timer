c7d94b9ef94e045e8f5e3c62bd9cac1cb6511483
André Porto <andreporto@me.com>
2023-03-28 17:29:37 -0300

    Typescript form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 
 import {
 	CountdownContainer,
@@ -11,10 +13,27 @@ import {
 	TaskInput,
 } from './styles';
 
-export function Home() {
-	const { register, handleSubmit, watch } = useForm();
+const newCycleFormValidationSchema = zod.object({
+	task: zod.string().min(1, 'Informe a atividade'),
+	owner: zod.string().optional(),
+	minutesAmount: zod
+		.number()
+		.min(5, 'O tempo mínimo é de 5 minutos.')
+		.max(60, 'O tempo máximo é de 60 minutos.'),
+});
 
-	function handleCreateNewCycle(data: any) {
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+export function Home() {
+	const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+		resolver: zodResolver(newCycleFormValidationSchema),
+		defaultValues: {
+			task: '',
+			minutesAmount: 0,
+		},
+	});
+
+	function handleCreateNewCycle(data: NewCycleFormData) {
 		console.log(data);
 	}
 
